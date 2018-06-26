bd2378f1b1492a27d7cc33883c54a195d14efcef
Feihong Hsu <feihong.hsu@gmail.com>
2018-06-26 15:10:47 -0500

    Fix the logic that maps URLs to .pug file paths

--- server.js
+++ server.js
@@ -6,21 +6,49 @@ const pug = require('pug')
 const app = express()
 app.use(express.static('public'))
 
-app.get('/:path(*)', async (req, res) => {
-  let path_ = path.join('public', req.params.path)
+// Map the URL to the actual file path of the .pug template. Return null if the
+// file doesn't exist.
+async function getActualPath(url) {
+  // Try the specific .pug file
+  let result = path.join('public', url) + '.pug'
+  if (await fileExists(result)) {
+    return result
+  }
+  // Pug file doesn't exist, try index.pug under directory
+  result = path.join('public', url, 'index.pug')
+  if (await fileExists(result)) {
+    return result
+  }
+  return null
+}
+
+// Return true if the file at the given path exists; false otherwise.
+async function fileExists(filePath) {
   try {
-    let stat = await fs.stat(path_)
-    if (stat.isDirectory()) {
-      path_ = path.join(path_, 'index.pug')
-    }
-    let html = pug.renderFile(path_)
-    res.send(html)
+    let stat = await fs.stat(filePath)
+    return true
   } catch (err) {
     if (err.code === 'ENOENT') {
-      res.status(404).send('File not found')
+      return false
     } else {
-      res.status(500).send(`Unexpected error: ${err.message}`)
+      throw err
     }
+  }
+}
+
+app.get('/:path(*)', async (req, res) => {
+  let path_ = await getActualPath(req.params.path)
+  console.log(path_);
+
+  if (path_ !== null) {
+    try {
+      let html = pug.renderFile(path_)
+      res.send(html)
+    } catch (err) {
+      res.status(500).send(`Unexpected error: <pre>${err.message}</pre>`)
+    }
+  } else {
+    res.status(404).send('File not found')
   }
 })
 
